466f2bb57c5e0848adf67d38d7a28a30db8b60c8
scnp <scnp@zhaw.ch>
2023-10-03 23:20:41 +0200

    add data edit functionality

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -2,6 +2,7 @@
 import { FirestoreService } from '../shared/services/firestore.service';
 import { AuthService } from 'src/app/shared/services/auth.service';
 import { User } from 'firebase/auth';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-user-profile',
@@ -11,28 +12,51 @@ import { User } from 'firebase/auth';
 export class UserProfileComponent implements OnInit {
   user: User | null = null; 
   data: any;
+  profileForm: FormGroup;
+  name: string | null = null;
+  state: string | null = null;
+  country: string | null = null;
 
-  constructor(private authService: AuthService, private firestoreService: FirestoreService) {}
+  constructor(private authService: AuthService, private firestoreService: FirestoreService, private formBuilder: FormBuilder,) {
+    this.profileForm = this.formBuilder.group({
+      name: "",
+      state: "",
+      country: ""
+    });
+  }
 
   ngOnInit() {
     this.authService.user$.subscribe((user) => {
       this.user = user;
-      if (this.user) {
-        this.firestoreService.getUserData(this.user.uid);
-      }
+      this.getData();
     });
     this.firestoreService.userData$.subscribe((userData) => {
       this.data = userData;
     });
   }
 
+  onSubmit() {
+    this.name = this.profileForm.value.name;
+    this.state = this.profileForm.value.state;
+    this.country = this.profileForm.value.country;
+    this.saveData()
+  }
+
   async saveData() {
     let uid = this.user?.uid ? this.user?.uid : "";
-    await this.firestoreService.saveUserData(uid, {name: "New York", state: "NY", country: "USA"});
+    await this.firestoreService.saveUserData(uid, {name: this.name, state: this.state, country: this.country});
+    await this.getData();
   }
   async getData() {
-    let uid = this.user?.uid ? this.user?.uid : "";
-    await this.firestoreService.getUserData(uid);
+    let uid = this.user?.uid
+    if (uid){
+      await this.firestoreService.getUserData(uid);
+      this.name = this.data.name;
+      this.state = this.data.state;
+      this.country = this.data.country;
+    } else {
+      console.log("no uid")
+    }
   }
 
 }
