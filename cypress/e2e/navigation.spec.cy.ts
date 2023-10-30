a363601bf4379bcc24d785a33a17a446da06b7e8
scnp <scnp@zhaw.ch>
2023-10-30 08:07:34 +0100

    add cypress and implement first e2e test

---
+++ cypress/e2e/navigation.spec.cy.ts
@@ -0,0 +1,30 @@
+describe('Navigation Tests', () => {
+    it('should navigate to different pages', () => {
+      // Visit the home page
+      cy.visit('/');
+  
+      // Find a navigation element (e.g., a link) and click it
+      cy.get('a[href="/favourites"]').click();
+  
+      // Check if the URL has changed to the target page (e.g., favourites page)
+      cy.url().should('include', '/login');
+  
+      // Check if the content specific to the about page is visible
+      cy.contains('Login').should('be.visible');
+  
+      // Go back to the previous page
+      cy.go('back');
+  
+      // Check if the URL has reverted back to the initial page
+      cy.url().should('not.include', '/favourites');
+  
+      // Check if the content specific to the initial page is visible
+      cy.contains('Where do you want to go').should('be.visible');
+  
+      // You can continue to add more navigation tests as needed.
+      cy.get('a[href="/profile"]').click();
+      cy.url().should('include', '/login');
+      cy.contains('a', 'Home').should('have.attr', 'href', '/').click();
+    });
+  });
+  
