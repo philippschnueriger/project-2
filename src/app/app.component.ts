05a1d876d6071d2f999b92f4d37ad5d48a64eccd
scnp <scnp@zhaw.ch>
2023-11-26 19:17:26 +0100

    directly login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,43 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { Firestore, collection, collectionData } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
+import { AuthService } from 'src/app/shared/services/auth.service';
+import { User } from 'firebase/auth';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss']
+  styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  firestore: Firestore = inject(Firestore)
+  firestore: Firestore = inject(Firestore);
   items$: Observable<any[]>;
   mobileNavOpen = false;
+  user: User | null = null;
+  error: string | null = null;
 
-  constructor() {
-    const aCollection = collection(this.firestore, 'items')
+  constructor(private authService: AuthService) {
+    const aCollection = collection(this.firestore, 'items');
     this.items$ = collectionData(aCollection);
+  }
+
+  ngOnInit() {
+    this.authService.user$.subscribe((user) => {
+      this.user = user;
+    });
   }
 
   toggleNav() {
     this.mobileNavOpen = !this.mobileNavOpen;
+  }
+
+  async logout() {
+    try {
+      await this.authService.logout()
+      this.error = null;
+    } catch (error: any) {
+      this.error = "unknown error"
+      console.log(error)
+    }
   }
 }
