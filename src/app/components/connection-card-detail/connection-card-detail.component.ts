90c85164254a215f411908101f68c1608141d65f
scnp <scnp@zhaw.ch>
2023-11-26 15:26:22 +0100

    connection card and detail component separated

--- src/app/components/connection-card-detail/connection-card-detail.component.ts
+++ src/app/components/connection-card-detail/connection-card-detail.component.ts
@@ -4,8 +4,15 @@ import { TripSummary } from '../connection-card/tripSummary';
 @Component({
   selector: 'app-connection-card-detail',
   templateUrl: './connection-card-detail.component.html',
-  styleUrls: ['./connection-card-detail.component.scss']
+  styleUrls: ['./connection-card-detail.component.scss'],
 })
 export class ConnectionCardDetailComponent {
-    @Input() item: any;
+  @Input() item: any;
+  @Input() tripSummary: any;
+
+  expand: boolean = false;
+
+  toggleExpand() {
+    this.expand = !this.expand;
+  }
 }
